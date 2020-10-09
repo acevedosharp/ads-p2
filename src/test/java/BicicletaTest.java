1a203568a5abad57d0c641c4bc0780035a62eca1
acevedosharp <acevedosharp@hotmail.com>
2020-10-09 15:16:36 -0500

    still mussing Empresa and Proxy

--- src/test/java/BicicletaTest.java
+++ src/test/java/BicicletaTest.java
@@ -1,2 +1,41 @@
+import entidades.BicicletaImpl;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BicicletaTest {
+
+    private final String serial = "XXX";
+    private final String marca = "Toyota";
+    private final String color = "Vantablack";
+
+    @Test
+    public void createSimpleBicicleta() {
+        BicicletaImpl bicicleta = new BicicletaImpl(serial, marca, color);
+
+        assertTrue(
+                bicicleta.getSerial().equals(serial) &&
+                        bicicleta.getMarca().equals(marca) &&
+                        bicicleta.getColor().equals(color)
+        );
+    }
+
+    @Test
+    public void editSimpleBicicleta() {
+        BicicletaImpl bicicleta = new BicicletaImpl(serial, marca, color);
+
+        String newSerial = "ZZZ";
+        String newMarca = "BMW";
+        String newColor = "Brown";
+
+        bicicleta.setSerial(newSerial);
+        bicicleta.setMarca(newMarca);
+        bicicleta.setColor(newColor);
+
+        assertTrue(
+                bicicleta.getSerial().equals(newSerial) &&
+                        bicicleta.getMarca().equals(newMarca) &&
+                        bicicleta.getColor().equals(newColor)
+        );
+    }
 }
--- src/test/java/BiciusuarioTest.java
+++ src/test/java/BiciusuarioTest.java
@@ -1,19 +1,67 @@
+import decorator.Bicicleta;
+import entidades.BicicletaImpl;
 import entidades.Biciusuario;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 
 public class BiciusuarioTest {
 
-    private String correo = "acevedosharp@hotmail.com";
-    private String password = "Password123";
-    private String nombre = "Jose";
-    private String direccion = "Barrancabermeja";
-    private String telefono = "30221752885";
-    private String id = "1005221892";
+    private final String correo = "acevedosharp@hotmail.com";
+    private final String password = "Password123";
+    private final String nombre = "Jose";
+    private final String direccion = "Barrancabermeja";
+    private final String telefono = "30221752885";
+    private final String id = "1";
 
     @Test
-    public void successfulCreation() throws Exception {
+    public void createSimpleBiciusuario() {
         Biciusuario biciusuario = new Biciusuario(correo, password, nombre, direccion, telefono, id);
+
+        assertTrue(
+                biciusuario.getCorreo().equals(correo) &&
+                        biciusuario.getContrasena().equals(password) &&
+                        biciusuario.getNombre().equals(nombre) &&
+                        biciusuario.getDireccion().equals(direccion) &&
+                        biciusuario.getTelefono().equals(telefono)
+        );
     }
 
+    @Test
+    public void editSimpleBicicleta() {
+        Biciusuario biciusuario = new Biciusuario(correo, password, nombre, direccion, telefono, id);
+
+        String newCorreo =  "joseacpo@unisabana.edu.co";
+        String newPass = "NewPass123";
+        String newNombre = "León de Greiff";
+        String newDireccion = "Chía";
+        String newTelefono = "3022175285";
+
+        biciusuario.setCorreo(newCorreo);
+        biciusuario.setContrasena(newPass);
+        biciusuario.setNombre(newNombre);
+        biciusuario.setDireccion(newDireccion);
+        biciusuario.setTelefono(newTelefono);
+
+        assertTrue(
+                biciusuario.getCorreo().equals(newCorreo) &&
+                        biciusuario.getContrasena().equals(newPass) &&
+                        biciusuario.getNombre().equals(newNombre) &&
+                        biciusuario.getDireccion().equals(newDireccion) &&
+                        biciusuario.getTelefono().equals(newTelefono)
+        );
+    }
+
+    @Test
+    public void anadirBicicletaYConsultar() {
+        Biciusuario biciusuario = new Biciusuario(correo, password, nombre, direccion, telefono, id);
+
+        Bicicleta bicicleta = new BicicletaImpl("XXX", "Toyota", "Vantablack");
+        biciusuario.agregarBicicleta(bicicleta);
+
+        boolean res = biciusuario.getBicicletas().contains(bicicleta);
+
+        assertTrue(res);
+    }
 }
