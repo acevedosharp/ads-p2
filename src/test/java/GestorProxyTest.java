5c20c8688dc3caf0c41495d7117e5f431d3db067
acevedosharp <acevedosharp@hotmail.com>
2020-10-09 15:40:06 -0500

    2first from proxy

--- src/test/java/GestorProxyTest.java
+++ src/test/java/GestorProxyTest.java
@@ -1,2 +1,52 @@
+import entidades.Biciusuario;
+import gestor.*;
+import org.junit.Test;
+
 public class GestorProxyTest {
+    private final String biciusuario1Id = "123";
+    private final String biciusuario1Correo = "acevedosharp@hotmail.com";
+    private final String biciusuario1Contrasena = "Password123";
+    private final String biciusuario1Nombre = "Jose Acevedo";
+    private final String biciusuario1Direccion = "Barrancabermeja";
+    private final String biciusuario1Telefono = "3022175285";
+
+    private final String biciusuario2Id = "456";
+    private final String biciusuario2Correo = "joseacpo@unisabana.edu.co";
+    private final String biciusuario2Contrasena = "Password123";
+    private final String biciusuario2Nombre = "León de Greiff";
+    private final String biciusuario2Direccion = "Barrancabermeja";
+    private final String biciusuario2Telefono = "3022175286";
+
+    private final String empresa1Nit = "321";
+    private final String empresa1Correo = "empresita@hotmail.com";
+    private final String empresa1Contrasena = "Passssss123";
+    private final String empresa1Nombre = "Empresita LLC";
+    private final String empresa1Direccion = "Bogotá D.C.";
+    private final String empresa1Telefono = "3022175287";
+
+    private final String empresa2Nit = "654";
+    private final String empresa2Correo = "empresovsky@hotmail.com";
+    private final String empresa2Contrasena = "Passssss123";
+    private final String empresa2Nombre = "Empresovsky LLC";
+    private final String empresa2Direccion = "Bogotá D.C.";
+    private final String empresa2Telefono = "3022175288";
+
+    public GestorProxyTest() throws IllegalEmailException, IllegalPasswordException {
+        GestorSingleton.build().crearBiciusuario(biciusuario1Id, biciusuario1Correo, biciusuario1Contrasena, biciusuario1Nombre, biciusuario1Direccion, biciusuario1Telefono);
+        GestorSingleton.build().crearBiciusuario(biciusuario2Id, biciusuario2Correo, biciusuario2Contrasena, biciusuario2Nombre, biciusuario2Direccion, biciusuario2Telefono);
+        GestorSingleton.build().crearEmpresa(empresa1Nit, empresa1Correo, empresa1Contrasena, empresa1Nombre, empresa1Direccion, empresa1Telefono);
+        GestorSingleton.build().crearEmpresa(empresa2Nit, empresa2Correo, empresa2Contrasena, empresa2Nombre, empresa2Direccion, empresa2Telefono);
+    }
+
+    @Test(expected = WrongCredentialsException.class)
+    public void userWithWrongCredentials() throws WrongCredentialsException {
+        new GestorProxy(new Biciusuario("c", "p", "n", "d", "t", "i"));
+    }
+
+    @Test
+    public void userWithCorrectCredentials() throws WrongCredentialsException {
+        new GestorProxy(GestorSingleton.build().consultarBiciusuario(biciusuario1Id));
+    }
+
+
 }
